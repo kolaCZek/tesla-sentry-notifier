2f7e543573b4b161fa642def6db180a559967f67
Martin Kolaci <mail@kolaczek.cz>
2024-07-06 00:08:20 +0200

    Added TIMER_SKIP and removed deprecated api calls (flash/honk)
    Signed-off-by: Martin Kolaci <mail@kolaczek.cz>

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import os
-import signal
-import time
 from datetime import datetime
-import pytz
-import logging
-import teslapy
-import requests
-import paho.mqtt.client as mqtt
 import json
+import logging
+import os
+import paho.mqtt.client as mqtt
+import pytz
+import requests
+import signal
+import teslapy
+import time
 
 log_format = os.environ.get("LOG_FORMAT", "%(asctime)s %(levelname)s: %(message)s")
 log_level = os.environ.get('LOG_LEVEL', 'INFO').upper()
@@ -16,11 +16,12 @@ timezone_str = os.environ.get('TZ', 'UTC')
 tesla_user = os.environ.get("TESLA_USER")
 cars_vin_filter = os.environ.get("CARS_VIN", "").upper()
 
-timer = os.environ.get("TIMER", 10)
+timer = int(os.environ.get("TIMER", 10))
+timer_skip = int(os.environ.get("TIMER_SKIP", 120))
 
 mqtt_enabled = os.environ.get("MQTT_ENABLED", "false").lower()
 mqtt_server = os.environ.get("MQTT_SERVER", "")
-mqtt_port = os.environ.get("MQTT_PORT", 1883)
+mqtt_port = int(os.environ.get("MQTT_PORT", 1883))
 mqtt_user = os.environ.get("MQTT_USER", "")
 mqtt_pass = os.environ.get("MQTT_PASS", "")
 mqtt_topic_prefix = os.environ.get("MQTT_TOPIC", "tesla-sentry")
@@ -29,9 +30,6 @@ ntfy_enabled = os.environ.get("NTFY_ENABLED", "false").lower()
 ntfy_server = os.environ.get("NTFY_SERVER", "https://ntfy.sh")
 ntfy_topic = os.environ.get("NTFY_TOPIC", "")
 ntfy_token = os.environ.get("NTFY_TOKEN", "")
-
-trigger_honk_horn = os.environ.get("HONK_HORN", "false").lower()
-trigger_flash_lights = os.environ.get("FLASH_LIGHTS", "false").lower()
 
 logging.basicConfig(format=log_format)
 logging.getLogger().setLevel(level=log_level)
@@ -42,7 +40,6 @@ if not os.path.isfile("/etc/tesla-sentry-notifier/cache.json"):
         json.dump({}, file)
 
 tesla = teslapy.Tesla(email=tesla_user, cache_file="/etc/tesla-sentry-notifier/cache.json")
-
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
 def current_time():
@@ -58,45 +55,36 @@ def get_vehicles():
 
     if cars_vin_filter:
         vehicles = [vehicle for vehicle in vehicles if 'vin' in vehicle and vehicle['vin'] in cars_vin_filter]
-        logging.info("Returning %s vehicles after VIN filter applied", len(vehicles))
+        logging.info("Returning %i vehicles after VIN filter applied", len(vehicles))
 
     for vehicle in vehicles:
         logging.debug("%s - %s", vehicle["vin"], vehicle["display_name"])
 
     return vehicles
 
-def is_sentry_active(vehicle):
-    vehicle_state = []
-
-    try:
-        vehicle_state = vehicle.get_vehicle_data()["vehicle_state"]
-    except requests.exceptions.HTTPError as err:
-        if err.response.status_code == 408:
-            logging.info('%s: vehicle is offline or asleep', vehicle['vin'])
-        else:
-            logging.error('HTTP %i %s', err.response.status_code, err.response.text)
-        return False
-
-
-    if "sentry_mode" not in vehicle_state or vehicle_state['sentry_mode'] == False:
-        logging.info('%s: sentry mode is off', vehicle['vin'])
-        return False
-
-    if "center_display_state" in vehicle_state and vehicle_state['center_display_state'] == 7:
-        logging.info('%s: sentry mode on - active', vehicle['vin'])
+def is_sentry_enabled(vehicle_state):
+    if "sentry_mode" in vehicle_state and vehicle_state['sentry_mode'] == True:
         return True
-    else:
-        logging.info('%s: sentry mode on - no activity', vehicle['vin'])
 
     return False
 
-def update_mqtt(vehicle, active):
+def is_sentry_triggered(vehicle_state):
+    if "center_display_state" in vehicle_state and vehicle_state['center_display_state'] == 7:
+        return True
+
+    return False
+
+def update_mqtt(vehicle):
+    if not mqttc.is_connected():
+        return False
+
     mqtt_topic = "{prefix}/{vin}/".format(prefix=mqtt_topic_prefix, vin=vehicle["vin"])
     logging.debug("MQTT: Updating topic %s", mqtt_topic)
 
-    mqttc.publish(mqtt_topic + "sentry_active", active)
+    mqttc.publish(mqtt_topic + "vehicle_online", vehicle["vehicle_online"])
+    mqttc.publish(mqtt_topic + "sentry_enabled", vehicle["sentry_enabled"])
+    mqttc.publish(mqtt_topic + "sentry_triggered", vehicle["sentry_triggered"])
     mqttc.publish(mqtt_topic + "last_update", current_time())
-
 
 def ntfy_send_message(vehicle):
     if not ntfy_server or not ntfy_topic:
@@ -140,8 +128,6 @@ def handle_sigterm(signum, frame):
 
 def main():
     vehicles = get_vehicles()
-    ntfy_message_sent = False
-    commands_sent = False
 
     if mqtt_server and mqtt_enabled == "true":
         if mqtt_user and mqtt_pass:
@@ -154,28 +140,64 @@ def main():
 
     try:
         while True:
+            current_time = time.time()
+
             for vehicle in vehicles:
-                logging.debug("%s: Getting status...", vehicle["vin"])
-                
-                sentry_active = is_sentry_active(vehicle)
+                if "skip" in vehicle and current_time < vehicle["skip"]:
+                    logging.debug('%s: Vehicle Offline or Senty OFF - Skipping (%i seconds)', vehicle['vin'], vehicle["skip"] - current_time)
+                    continue
 
-                if commands_sent == False and sentry_active:
-                    if trigger_flash_lights == "true":
-                        logging.debug("%s: Flashing lights", vehicle["vin"])
-                        vehicle.command('FLASH_LIGHTS')
-                    if trigger_honk_horn == "true":
-                        logging.debug("%s: Honking horn", vehicle["vin"])
-                        vehicle.command('HONK_HORN')
-                commands_sent = sentry_active
+                vehicle["vehicle_online"] = False
+                vehicle["sentry_enabled"] = False
+                vehicle["sentry_triggered"] = False
 
-                if mqtt_enabled == "true" and mqttc.is_connected():
-                    update_mqtt(vehicle, sentry_active)
+                try:
+                    logging.debug("%s: Getting status...", vehicle["vin"])
+                    vehicle_state = vehicle.get_vehicle_data()["vehicle_state"]
+                except requests.exceptions.HTTPError as err:
+                    if err.response.status_code == 408:
+                        logging.info('%s: Vehicle is Offline or Sleeping - Skipping for %i seconds', vehicle['vin'], timer_skip)
+                        vehicle["skip"] = current_time + timer_skip
+                    else:
+                        logging.error('HTTP %i %s', err.response.status_code, err.response.text)
+                    if mqtt_enabled == "true":
+                        update_mqtt(vehicle)
+                    continue
+
+                vehicle["vehicle_online"] = True
+
+                if is_sentry_enabled(vehicle_state):
+                    vehicle["sentry_enabled"] = True
+                else:
+                    logging.info('%s: Sentry Mode OFF - Skipping this vehicle for %i seconds', vehicle['vin'], timer_skip)
+                    vehicle["skip"] = current_time + timer_skip
+                    if mqtt_enabled == "true":
+                        update_mqtt(vehicle)
+                    continue
+
+                if is_sentry_triggered(vehicle_state):
+                    logging.info('%s: Sentry Mode ON - Triggered', vehicle['vin'])
+                    vehicle["sentry_triggered"] = True
+                else:
+                    logging.info('%s: Sentry Mode ON - No Activity', vehicle['vin'])
+                    vehicle["commands_sent"] = False
+                    vehicle["ntfy_message_sent"] = False
+                    if mqtt_enabled == "true":
+                        update_mqtt(vehicle)
+                    continue
+
+                if mqtt_enabled == "true":
+                    update_mqtt(vehicle)
+
+                if not "ntfy_message_sent" in vehicle:
+                    vehicle["ntfy_message_sent"] = False
+
                 if ntfy_enabled == "true":
-                    if ntfy_message_sent == False and sentry_active:
-                        ntfy_send_message(vehicle, sentry_active)
-                    ntfy_message_sent = sentry_active
+                    if vehicle["ntfy_message_sent"] == False:
+                        ntfy_send_message(vehicle)
+                        vehicle["ntfy_message_sent"] = True
 
-            time.sleep(int(timer))
+            time.sleep(timer)
 
     except KeyboardInterrupt:
         logging.info("Interrupted by user input")
